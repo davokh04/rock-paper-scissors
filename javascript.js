0d87d4cf8950b66385d726319a09d7bacc17bceb
David <davkh10@gmail.com>
2024-09-09 16:48:58 -0300

    Create getHumanchoice(), playRound() and variables

--- javascript.js
+++ javascript.js
@@ -1,10 +1,32 @@
 function getComputerChoice() {
     let choice = Math.random();
     if (choice >= 0 && choice < 0.333) {
-        return "rock"
-    } else if (choice >= 0.333 && choice < 0.666)  {
-        return "paper"
-    } return "scissors"
+        return "ROCK";
+    } else if (choice >= 0.333 && choice < 0.666) {
+        return "PAPER";
+    }   return "SCISSORS";
 }
 
+function getHumanChoice() {
+    let choice = prompt("Put your choice! Rock, paper or scissors.");
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();
+    if (humanChoice == "ROCK" && computerChoice == "PAPER") {
+        console.log(`You lose! ${computerChoice.capitalize()} beats ${humanChoice.capitalize()}`);
+    } else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
+        console.log(`You lose! ${computerChoice.capitalize()} beats ${humanChoice.capitalize()}`);
+    } else if (humanChoice == "SCISSORS" && computerChoice == "ROCK") {
+        console.log(`You lose! ${computerChoice.capitalize()} beats ${humanChoice.capitalize()}`);
+    } else if (humanChoice == computerChoice) {
+        console.log('It is a tie.')
+    } else {
+        console.log(`You win! ${computerChoice.capitalize()} beats ${humanChoice.capitalize()}`)
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
 
