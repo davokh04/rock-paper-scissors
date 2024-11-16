1db6e4e8e06539e1c7bac4029c19723f3054bca2
David <davkh10@gmail.com>
2024-11-15 21:04:31 -0300

    Add getHumanChoice function

--- js.js
+++ js.js
@@ -1,9 +1,13 @@
 function getComputerChoice() {
-    const n = Math.random() * 10;
+    let n = Math.random() * 10;
     if (n >= 1 && n < 4) {
         return 'ROCK';
     } else if (n >= 4 && n < 7) {
         return 'PAPER';
     } return 'SCISSORS';
+}
+
+function getHumanChoice() {
+    return prompt('Choice:', 'Rock, Paper or Scissors...')
 }
 
